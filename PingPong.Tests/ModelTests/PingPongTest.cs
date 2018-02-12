3f8216337fd69d1bd7dde7fb74dec9831535469d
Kayla Ondracek and Michael Albers <michael@>
2018-02-12 10:08:48 -0800

    add method to take input and return list count up to that input; write test for that method

--- PingPong.Tests/ModelTests/PingPongTest.cs
+++ PingPong.Tests/ModelTests/PingPongTest.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using PingPong.Models;
+using System;
 
 namespace PingPong.Tests
 {
   [TestClass]
-  public class PingPongGeneratorTest
+  public class PingPongGeneratorTest : IDisposable
   {
+    public void Dispose()
+    {
+      PingPongGenerator.ClearAll();
+    }
+
     [TestMethod]
     public void GetInput_ReturnInput_Int()
     {
@@ -20,6 +27,24 @@ namespace PingPong.Tests
       Assert.AreEqual(userInput, result);
     }
 
+    [TestMethod]
+    public void CountInput_ReturnCountedInput_List()
+    {
+      //Arrange
+      List<int> greatList = new List<int> {1, 2, 3, 4, 5};
+      int userInput = 5;
+      PingPongGenerator newPong = new PingPongGenerator(userInput);
+
+      //Act
+      List<int> result = newPong.CountInput(userInput);
+
+      //Assert
+      CollectionAssert.AreEqual(greatList, result);
+    }
+
+    [TestMethod]
     
+
+
   }
 }
--- PingPong/Models/PingPongGenerator.cs
+++ PingPong/Models/PingPongGenerator.cs
@@ -1,19 +1,36 @@
 using System.Collections.Generic;
+using System;
 
 namespace PingPong.Models
 {
   public class PingPongGenerator
   {
     private int _input;
+    private static List<PingPongGenerator> _instances = new List<PingPongGenerator> {};
 
     public PingPongGenerator(int input)
     {
       _input = input;
+      _instances.Add(this);
     }
 
     public int GetInput()
     {
       return _input;
+    }
+
+    public List<int> CountInput(int input)
+    {
+      List<int> userList = new List<int> ();
+      for (int i = 1; i <= input; i++) {
+        userList.Add(i);
+      }
+      return userList;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
